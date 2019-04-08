0669c1315705c58970afb6f63de44136cffcf58e
Roman <Romazho123@gmail.com>
2019-04-07 23:33:46 -0400

    ajout

--- data/TP5/src/Dijkstra.java
+++ data/TP5/src/Dijkstra.java
@@ -24,7 +24,6 @@ public class Dijkstra {
 		
 		// A complÃ©ter
 		
-		boolean trouve = false;
 		final int INFINITY = 99999;
 		
 		for(int i=0;i < graph.getNodes().size();++i) {
@@ -33,54 +32,108 @@ public class Dijkstra {
 
 		s.distance = 0;
 		s.found = true;
-		int currentLength = 0;
 		
-		//dijkstraTable[0].put(s, arg1)
+		//la liste avec les edges de la source
+		ArrayList<Edge> list = new ArrayList<Edge>(graph.getEdgesGoingFrom(s));
 		
-		while(trouve == false) {
-			//la liste avec les edges de la source
-			ArrayList<Edge> list = new ArrayList<Edge>(graph.getEdgesGoingFrom(s));
+		//l'update autour du noeud source
+		for(int i=0;i<list.size();++i) {
+			List<Node> tmpNodeList = graph.getNodes();
+			int tmpIndex = tmpNodeList.indexOf(list.get(i));
+			// On update la distance des noeuds qui sont la destination du noeud source
+			// modifie le node et non le edge
+			tmpNodeList.get(tmpIndex).distance = list.get(i).getDistance() + s.distance;
+		}
 		
+		
+		//int currentLength = 0;
+		
+		
+		
+		int iteration = 0;
+		while(d.found == false) {
 			
-			//l'update
-			for(int i=0;i<list.size();++i) {
-				List<Node> tmpNodeList = graph.getNodes();
-				int tmpIndex = tmpNodeList.indexOf(list.get(i));
-				// On update la distance des noeuds qui sont la destination du noeud source
-				tmpNodeList.get(tmpIndex).distance = list.get(i).getDistance() + currentLength;// modifie le node et non le edge
-			}
 			int min = INFINITY;
 			
 			// Trouver le prochain noeud
 			Node bestNode = new Node();
 			int bestNodeIndex = 0;
-			int i =0;
-			
-			
-			
-			for(Node node : graph.getNodes()) {
-				if(!node.found && node.distance < min) {
-					min = node.distance;
-					bestNode = node; // El bestos nodos
-					bestNodeIndex =i;
+			{	int i = 0;
+				//on parcours tous les nodes
+				for(Node node : graph.getNodes()) {
+					//si le node n'est pas dans l'ensemble et si sa valeur est plus petite
+					if( (node.found == false) && (node.distance < min) ) {
+						min = node.distance;
+						bestNode = node; // El bestos nodos
+						bestNodeIndex = i;
+					}
+					i++;
 				}
-				i++;
-			}
+			}			
+
 			
+			//le node trouvé fait partie de l'ensemble.
 			graph.getNodes().get(bestNodeIndex).found = true;
 			
+			Edge bestEdge = new Edge();
+			
+			//il faut trouver le edge min et l'ajouté au path
+			//pas bon car il faut parcourir tous les edges our tous les nodes
+			for(int i = 0; i<list.size(); i++) {
+				if(list.get(i).getDestination() == graph.getNodes().get(bestNodeIndex))
+					bestEdge = list.get(i);
+			}
+			
+			//put DestinationNode, edge
+			dijkstraTable[iteration].put(bestNode, bestEdge);
+			
+			//ajout du edge au path
+			//faire un while
+			int iteration2 = iteration;
+			while( bestEdge.getSource() != dijkstraTable[iteration2].get(bestNode).getSource()) {
+				path.pop();
+				iteration2--;
+			}
+			
+			do {
+				path.add(bestEdge);
+			}
+			while( bestEdge.getSource() == dijkstraTable[iteration].get(bestNode).getSource());
+			
+			
+			/*//on choisi le meilleur edge
 			//Pas sur s'il faut parcourir la liste
 			for(Edge edge : list) {
 				//A-> 		B 					bestNode
 				if(edge.getDestination() == graph.getNodes().get(bestNodeIndex)) {// Trouver le edge en fction du node
-					edge.
 					currentLength += edge.getDistance();
 					break;
 				}
+			}*/
+			
+			//la liste avec les edges du bestNode
+			list = new ArrayList<Edge>(graph.getEdgesGoingFrom(bestNode));
+			
+			//l'update
+			for(int i=0;i<list.size();++i) {
+				//List<Node> tmpNodeList = graph.getNodes();
+				//retourne l'index du noeud de destination
+				int tmpIndex = graph.getNodes().indexOf(list.get(i).getDestination());
+				// On update la distance des noeuds qui sont la destination du noeud source
+				// modifie le node et non le edge
+				
+				//il faut vérifier si la nouvelle somme est plus petite et si oui on update.
+				int distanceEdge = list.get(i).getDistance();
+				int distanceNoeud = bestNode.distance;
+				if( (distanceEdge + distanceNoeud) < graph.getNodes().get(tmpIndex).distance)
+					graph.getNodes().get(tmpIndex).distance = list.get(i).getDistance() + bestNode.distance;
 			}
 			
+			iteration++;
+			///////////////////////////////dijkstra fini///////////////////////////////
 			
-			//on choisi le meilleur edge
+			//ajout du edge dans le path
+			
 			
 			
 			
